e9afa21b38732ed1538385b3b3e2383250dc0e87
Mohamed Abdinasir <mabdinasira@gmail.com>
2025-03-12 10:43:58 +1100

    Fix PM2 config to use CommonJS

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,13 +1,13 @@
 /* eslint-disable camelcase */
 
-export default {
+module.exports = {
     apps: [
         {
             name: 'eastleighrealestate',
-            script: 'dist/main.js',
+            script: './dist/main.js',
             instances: 'max',
             exec_mode: 'cluster',
-            watch: true,
+            watch: false,
         },
     ],
 }
