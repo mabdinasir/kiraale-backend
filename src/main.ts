44934702d2c56c8e3de0ffa4287ca93c87ac7129
Mohamed Abdinasir <mabdinasira@gmail.com>
2025-03-23 13:35:28 +1100

    added PORT to CI

--- src/main.ts
+++ src/main.ts
@@ -17,7 +17,7 @@ app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }))
 app.use(cookieParser())
 
 app.get('/', (req, res) => {
-    res.send('Testing new auto deployment')
+    res.send('Testing new auto deployment - PORT: ' + port)
 })
 
 const router = express.Router()
