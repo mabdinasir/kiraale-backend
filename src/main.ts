eb60e3edd52b24f64a55c68dc290be21aa4087ba
Mohamed Abdinasir <mabdinasira@gmail.com>
2025-04-27 14:54:48 +1000

    add rate limiter and helmet

--- src/main.ts
+++ src/main.ts
@@ -5,13 +5,17 @@ import express, { type Express } from 'express'
 import bodyParser from 'body-parser'
 import configureRoutes from '@routes/index'
 import { setupCrons } from '@lib/cron'
+import { apiRateLimiter } from '@middleware/rateLimiter'
+import helmet from 'helmet'
 
 dotenv.config()
 
 const port = process.env.PORT
 const app: Express = express()
 
+app.use(helmet())
 app.use(cors())
+app.use(apiRateLimiter)
 app.use(express.json())
 app.use(bodyParser.json({ limit: '50mb', type: 'application/json' }))
 app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }))
